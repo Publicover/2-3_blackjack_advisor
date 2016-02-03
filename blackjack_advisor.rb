b12bcae08862d27df839feb5a8df751acc7e81db
Publicover <jameslpublicover@gmail.com>
2016-02-03 15:14:58 -0500

    Added new subhash with its own default value

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -25,13 +25,56 @@ hard_hash["5"] = {"2" => "HIT" , "3" => "HIT" , "4" => "HIT" , "5" => "HIT" ,
 =end
 
 
+# ************ USE SUB ARRAY WITH ITS OWN DEFAULT VALUE **************
+
 hard_hash = Hash.new("HIT")
+hard_hash_sub1 = Hash.new("STAND")
 
 hard_hash["17"] =
   {
     "2" => "STAND" , "3" => "STAND" , "4" => "STAND" ,
   "5" => "STAND" , "6" => "STAND" , "7" => "STAND" , "8" => "STAND" ,
   "9" => "STAND" , "10" => "STAND" , "A" => "STAND"}
+
+hard_hash["8"] = {
+    "5" => "DOUBLE OR HIT" , "6" => "DOUBLE OR HIT"
+}
+
+hard_hash["9"] = {
+  "2" => "DOUBLE OR HIT" , "3" => "DOUBLE OR HIT" , "4" => "DOUBLE OR HIT" ,
+  "5" => "DOUBLE OR HIT" , "6" => "DOUBLE OR HIT"
+}
+
+hard_hash["10"] = {
+  "2" => "DOUBLE OR HIT" , "3" => "DOUBLE OR HIT" , "4" => "DOUBLE OR HIT" ,
+  "5" => "DOUBLE OR HIT" , "6" => "DOUBLE OR HIT" , "7" => "DOUBLE OR HIT" ,
+  "8" => "DOUBLE OR HIT" , "9" => "DOUBLE OR HIT"
+}
+
+
+hard_hash["5"] = hard_hash_sub1
+=begin
+"6"
+"7"
+"8"
+"9"
+"10"
+"11"
+"12"
+"13"
+"14"
+"15"
+"16"
+"17"
+"18"
+"19"
+"20"
+"21"
+=end
+
+
+
+
 
 
 
