416ac43b37ab5a3e23e9b631eb37c8c248d09e06
Publicover <jameslpublicover@gmail.com>
2016-02-03 16:20:05 -0500

    Fixed typo where I wasn't defining hashes before editing them.

--- blackjack_advisor2.rb
+++ blackjack_advisor2.rb
@@ -54,16 +54,35 @@ hard_hash["10"] = {
 
 # ************ USE SUB ARRAY WITH ITS OWN DEFAULT VALUE **************
 
-hard_hash = Hash.new("STAND")
-hard_hash_sub1 = Hash.new("HIT")
-hard_hash_sub2 = Hash.new("DOUBLE OR HIT")
+hard_hash_STAND = Hash.new("STAND")
+hard_hash_HIT = Hash.new("HIT")
+hard_hash_DUB = Hash.new("DOUBLE OR HIT")
 
-hard_hash["5"] = hard_hash_sub1
-hard_hash["6"] = hard_hash_sub1
-hard_hash["7"] = hard_hash_sub1
-hard_hash["8"] = hard_hash_sub1
-hard_hash["8"]["5"] = "Double"
-hard_hash["8"]["6"] = "Double"
+hard_hash_STAND["5"] = hard_hash_HIT
+hard_hash_STAND["5"] = hard_hash_HIT
+hard_hash_STAND["7"] = hard_hash_HIT
+hard_hash_STAND["8"] = hard_hash_HIT
+hard_hash_STAND["8"]["5"] = "Double"
+hard_hash_STAND["8"]["6"] = "Double"
+hard_hash_STAND["9"] = hard_hash_HIT
+hard_hash_STAND["9"]["2"] = "Double"
+=begin
+hard_hash_STAND["9"]["3"] = "Double"
+hard_hash_STAND["9"]["4"] = "Double"
+hard_hash_STAND["9"]["5"] = "Double"
+hard_hash_STAND["9"]["6"] = "Double"
+hard_hash_STAND["10"]["2"]
+hard_hash_STAND["10"]["3"]
+hard_hash_STAND["10"]["4"]
+hard_hash_STAND["10"]["5"]
+hard_hash_STAND["10"]["6"]
+hard_hash_STAND["10"]["7"]
+hard_hash_STAND["10"]["8"]
+hard_hash_STAND["10"]["9"]
+hard_hash_STAND["11"] = hard_hash_DUB
+hard_hash_STAND["12"]["2"] = "HIT"
+hard_hash_STAND["12"]["3"] = "HIT"
+
 
 #  "5" => "DOUBLE OR HIT" , "6" => "DOUBLE OR HIT"
 
@@ -94,7 +113,7 @@ my_total_number = my_total_number.to_s
 puts "What does the dealer have?"
 dealer_card = gets.chomp
 
-puts hard_hash[my_total_number][dealer_card]
+puts hard_hash_STAND[my_total_number][dealer_card]
 
 =begin
 hash = {"Mason" => {"home" =>"919",
